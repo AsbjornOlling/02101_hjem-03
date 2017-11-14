526265c43b91817c171383156f5a8bc3d1f34c90
AsbjornOlling <asbjornolling@gmail.com>
2017-11-14 12:10:38 +0100

    Added template files from codejudge

---
+++ PeberholmDriver.java
@@ -0,0 +1,8 @@
+public class PeberholmDriver {
+	public static void main(String[] args) {
+		//PeberholmSimulation phs = new PeberholmSimulation(6);
+		PeberholmSimulationChecker phs = new PeberholmSimulationChecker ();
+	}
+	
+
+}
---
+++ Plane.java
@@ -0,0 +1,43 @@
+
+
+
+public class Plane {
+	
+	
+	private static final int DEFAULT_ID = 0;
+	
+	protected String manufacturer, type;
+	int id;
+	/**
+	 * @param manufacturer
+	 * @param type
+	 * @param id
+	 */
+	public Plane(String manufacturer, String type) {
+		this.manufacturer = manufacturer;
+		this.type = type;
+		this.id = DEFAULT_ID;
+	}
+	
+	
+	
+	public void setId(int id){
+		this.id = id;
+	}
+	
+	public String toString(){
+		// din kode 
+	}
+	//denne metode skal sammenligne id nr på to objekter af flyver.
+	
+	public boolean equals(Object o){
+		// din kode 
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	
+	
+}
