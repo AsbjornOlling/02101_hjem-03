a535e708e0d58791735a492940e84b1253e8d720
AsbjornOlling <asbjornolling@gmail.com>
2017-11-21 23:49:34 +0100

    fixed bug in make_plants

--- peberholm/make_plants.sh
+++ peberholm/make_plants.sh
@@ -5,14 +5,14 @@ cp plant_template.java Flower.java
 cp plant_template.java Moss.java
 cp plant_template.java Bush.java
 
-sed -i 's/Type/Tree/g' Tree.java
-sed -i 's/TYPE/TREE/g' Tree.java
+sed -i 's/<Type>/<Tree>/g' Tree.java
+sed -i 's/<TYPE>/<TREE>/g' Tree.java
 
-sed -i 's/Type/Flower/g' Flower.java
-sed -i 's/TYPE/FLOWER/g' Flower.java
+sed -i 's/<Type>/<Flower>/g' Flower.java
+sed -i 's/<TYPE>/<FLOWER>/g' Flower.java
 
-sed -i 's/Type/Moss/g' Moss.java
-sed -i 's/TYPE/MOSS/g' Moss.java
+sed -i 's/<Type>/<Moss>/g' Moss.java
+sed -i 's/<TYPE>/<MOSS>/g' Moss.java
 
-sed -i 's/Type/Bush/g' Bush.java
-sed -i 's/TYPE/BUSH/g' Bush.java
+sed -i 's/<Type>/<Bush>/g' Bush.java
+sed -i 's/<TYPE>/<BUSH>/g' Bush.java
