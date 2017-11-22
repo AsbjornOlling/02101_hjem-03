fca5eee20e05c8058efa84868bc9c0f920e96918
AsbjornOlling <asbjornolling@gmail.com>
2017-11-22 21:23:56 +0100

    first draft of two main methods in Airport

--- Airport.java
+++ Airport.java
@@ -1,5 +1,29 @@
+import java.util.ArrayList;
+
 public class Airport {
-	public void 	land (Plane plane) {
-		
+	private static ArrayList<Plane> landedPlanes = new ArrayList<Plane>();
+	private static int planesLanded;
+
+	// assign an id, and add to list of landed planes
+	public void	land (Plane plane) {
+		planesLanded++;
+		plane.id = planesLanded;
+		landedPlanes.add(plane);
 	}
-}
+
+
+	// removes plane from list of landed planes
+	public void start(int id) {
+		// search the list for plane matching id
+		int index = 0; // count indices manually in for-each lop
+		for(Plane plane : landedPlanes ) {
+			if (plane.id == id) {
+				landedPlanes.remove(index);
+			}
+			index++;
+		} // loop
+
+	} // start
+
+
+} // class
