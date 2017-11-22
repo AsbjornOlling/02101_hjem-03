34aa2060553071758c259b7dc7033b67430d5409
Hvidtrold <dingotheblue@gmail.com>
2017-11-22 15:12:20 +0100

    FreightPlane ver1

---
+++ Airport.java
@@ -0,0 +1,5 @@
+public class Airport {
+	public void 	land (Plane plane) {
+		
+	}
+}
---
+++ FreightPlane.java
@@ -0,0 +1,16 @@
+
+public class FreightPlane extends Plane {
+
+	private int payload;
+	
+	public FreightPlane(String manufacturer, String type, int payload) {
+		super(manufacturer, type);
+		this.payload = payload;
+	}
+	
+	public String toString(){
+		String planeString;
+		planeString = "Plane"+id+" "+manufacturer+" "+type+" "+"payload:"+payload;
+		return planeString;
+	}
+}
