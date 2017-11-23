ac1d095638e54f9b30355447d60dfc1b92f5331a
Hvidtrold <dingotheblue@gmail.com>
2017-11-23 10:46:13 +0100

    Airport mangler id og pæn string-output

--- Airport.java
+++ Airport.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+
 public class Airport {
+	
+	//The first method adds a plane to the those on the air port, and an assigns an a new id-number to that plane.
+	//The id-numbers start with 1 and are then increased by 1.
+	//If a plane wants to land which already has the id-number of plane on the airport, than is is not added.
+	
+	static ArrayList<String> planeList = new ArrayList<String>();
+	
 	public void 	land (Plane plane) {
+		planeList.add(plane.toString());
+	}
+	
+	//The second method removes the plane plane from those on the airport if it was actually there.
+	public void start (int id) {
+		
+	}
+	
+	//The last method list all planes currently on the airport, one per line in order of the landings.
+	//lige nu printer den Arraylist direkte
+	public String toString () {
+		String stringPlaneList = planeList.toString();
+		return stringPlaneList;
 		
 	}
 }
--- PlaneTester.java
+++ PlaneTester.java
@@ -2,7 +2,7 @@
 public class PlaneTester {
 	
 	public static void main (String[] args) {
-
+		/*
 	    Plane p1 = new Plane("Cessna","Skyhawk");
 	    Plane p2 = new Plane("Piper","Matix");
 	    Plane p3 = new Plane("Piper","Matix");
@@ -39,5 +39,36 @@ public class PlaneTester {
         System.out.println(p6);
         System.out.println(p4);
         System.out.println(p5);
+        */
+        // Airport-test
+        Airport cph = new Airport();
+        
+        Plane p1 = new Plane("Cessna", "Skyhawk");
+        PassengerPlane p2 = new PassengerPlane("Boing", "737", 241);
+        FreightPlane p3 = new FreightPlane("Antonov", "124", 110);
+        PassengerPlane p4 = new PassengerPlane("Boing", "737", 241);
+        PassengerPlane p5 = new PassengerPlane("Trash", "123", 99);
+        p5.setId(2);
+ 
+        cph.land(p1);
+        cph.land(p2);
+        cph.land(p3);
+        cph.land(p4);
+        System.out.println(cph);
+ 
+        cph.land(p5);
+        System.out.println(cph);
+ 
+        cph.start(2);
+        System.out.println(cph);
+ 
+        cph.start(0);
+        System.out.println(cph);
+        cph.start(5);
+        System.out.println(cph);
+        cph.start(1);
+        System.out.println(cph);
+        cph.start(3);
+        System.out.println(cph);
 	}
 }
